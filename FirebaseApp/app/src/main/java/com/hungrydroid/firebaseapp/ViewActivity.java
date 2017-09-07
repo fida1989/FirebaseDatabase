8b800bd123451702b0ed6909225167a59fac8c64
Fida Muntaseer <fida1989@users.noreply.github.com>
2017-09-07 21:37:17 +0600

    Added Delete Option

--- FirebaseApp/app/src/main/java/com/hungrydroid/firebaseapp/ViewActivity.java
+++ FirebaseApp/app/src/main/java/com/hungrydroid/firebaseapp/ViewActivity.java
@@ -2,7 +2,10 @@ package com.hungrydroid.firebaseapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,16 +19,18 @@ public class ViewActivity extends AppCompatActivity {
 
     //TextView tv;
     ListView listView;
-
-
+    ArrayList<String> arrayOfIds;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
         //tv = (TextView)findViewById(R.id.textView);
         listView = (ListView) findViewById(R.id.lv);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message").child("users");
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message").child("users");
+        //myRef.removeValue().
         ;
         // Read from the database
         myRef.addValueEventListener(new ValueEventListener() {
@@ -36,14 +41,15 @@ public class ViewActivity extends AppCompatActivity {
                 //System.out.println(dataSnapshot.toString());
                 //System.out.println(dataSnapshot.getKey());
                 ArrayList<User> arrayOfUsers = new ArrayList<User>();
+                arrayOfIds = new ArrayList<String>();
                 //String value = dataSnapshot.getValue(String.class);
                 for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
                     User u = singleSnapshot.getValue(User.class);
 
                     arrayOfUsers.add(u);
-                    System.out.println(u.getEmail());
+                    arrayOfIds.add(singleSnapshot.getKey());
+                    System.out.println(singleSnapshot.getKey());
                 }
-
 
                 UsersAdapter adapter = new UsersAdapter(ViewActivity.this, arrayOfUsers);
                 listView.setAdapter(adapter);
@@ -57,6 +63,19 @@ public class ViewActivity extends AppCompatActivity {
                 //Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                //Toast.makeText(ViewActivity.this,arrayOfIds.get(position)+"",Toast.LENGTH_SHORT).show();
+                myRef.child(arrayOfIds.get(position)).removeValue();
+                return false;
+            }
+        });
+
+
+        Toast.makeText(ViewActivity.this,"Long Press To Delete!",Toast.LENGTH_SHORT).show();
+
     }
 
 
